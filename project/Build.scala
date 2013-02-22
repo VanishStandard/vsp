ae2f561266f55106e3c3dac36ebf0d12f68eebe6
VanishStandard <github@v-standard.com>
2013-02-22 10:34:02 +0900

    publish-local を Local Maven Repositry に設定。

--- project/Build.scala
+++ project/Build.scala
@@ -15,6 +15,11 @@ object ApplicationBuild extends Build {
 			organization := appOrganization,
 			version := appVersion,
 			scalaVersion := buildScalaVersion,
+			publishMavenStyle := true,
+			otherResolvers := Seq(Resolver.file("dotM2", file(Path.userHome + "/.m2/repository"))),
+			publishLocalConfiguration <<= (packagedArtifacts, deliverLocal, ivyLoggingLevel) map {
+				(arts, _, level) => new PublishConfiguration(None, "dotM2", arts, List[String](), level)
+			},
 			libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test",
 			libraryDependencies <+= scalaVersion {
 				"org.scala-lang" % "scala-actors" % _
