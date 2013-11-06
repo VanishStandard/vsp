8470bddbe637a7a270ae1268ec5aaf12808dfd45
VanishStandard <github@v-standard.com>
2013-11-06 13:01:01 +0900

    バージョンアップ。

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 
 object ApplicationBuild extends Build {
 	val appName = "vsp"
-	val appVersion = "0.5.0"
+	val appVersion = "0.6.0-SNAPSHOT"
 	val appOrganization = "com.v_standard.vsp"
 	val buildScalaVersion = "2.10.0"
 
