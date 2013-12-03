5f2421eb4111de06ff10b2f2022c59ecafeb4ff8
Mt.sito <si.dev.ml@gmail.com>
2013-12-03 16:46:53 +0900

    バージョンアップ。

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 
 object ApplicationBuild extends Build {
 	val appName = "vsp"
-	val appVersion = "0.6.3-SNAPSHOT"
+	val appVersion = "0.6.3"
 	val appOrganization = "com.v_standard.vsp"
 	val buildScalaVersion = "2.10.3"
 
